a457002ece53d3487ba9b75baca51067107f1338
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-10-07 21:40:14 -0700

    Bump to 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             group = "so.dang.cool"
-            version = "1.0.0"
+            version = "1.0.1"
             artifactId = "levee"
 
             from(components["java"])
